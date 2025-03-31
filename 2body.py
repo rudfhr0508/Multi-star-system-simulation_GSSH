a8dadf55fcfbef2b8c39db240a47be0510dd32f8
rudfhr0508 <131419511+rudfhr0508@users.noreply.github.com>
2025-03-31 11:12:16 +0900

    Add files via upload

--- 2body.py
+++ 2body.py
@@ -17,8 +17,8 @@ p1 = np.array([0.5, 0.0, 0.0])
 p2 = np.array([-0.5, 0.0, 0.0]) 
 
 # 초기 속도 
-v1 = np.array([0.0, 0.5, 0.0])   
-v2 = np.array([0.0, -0.5, 0.0])  
+v1 = np.array([0.0, np.sqrt(2)/2, 0.0])
+v2 = np.array([0.0, -np.sqrt(2)/2, 0.0])
 
 initial_conditions = np.array([p1, p2, v1, v2]).ravel()
 
